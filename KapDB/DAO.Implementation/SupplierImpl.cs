1ef690b27fb42d5f53eff6552dd597f8aa7a96d9
PedroConde20 <cvp2019844@est.univalle.edu>
2023-05-26 14:19:33 -0400

    Cambios supplier

---
+++ KapDB/DAO.Implementation/SupplierImpl.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO.Model;
+using DAO.interfaces;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace DAO.Implementation
+{
+    public class SupplierImpl : ISupplier
+
+    {
+        public int Delete(Supplier t)
+        {
+            int res = 0;
+            System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1} Inicializando el Metodo Delete de la Tabla Supplier", DateTime.Now, Session.SessionID));
+            string query = @"UPDATE Supplier SET status=0
+                             WHERE supplierID = @supplierID";
+
+            try
+            {
+                SqlCommand command = DataBase.CreateBasicCommand(query);
+                command.Parameters.AddWithValue("@supplierID", t.SupplierID);
+                res = DataBase.ExecuteBasicCommand(command);
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1}  Metodo Delete Ejecutado Correctamente", DateTime.Now, Session.SessionID));
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1} Error en el metodo Delete de la Tabla Supplier - {2} - {3}", DateTime.Now, Session.SessionID, ex.Message, query));
+                throw ex;
+            }
+            return res;
+        }
+
+        public Supplier Get(byte id)
+        {
+            Supplier t = null;
+            System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1} Inicializando el Metodo Get de la Tabla Supplier", DateTime.Now, Session.SessionID));
+            string query = @"SELECT supplierID,supplierName,supplierLastName,supplierCI,direction,cellphone,email,UserID,registerDate
+		                    ,lastUpdate,status
+                            FROM Supplier
+                            WHERE supplierID = @supplierID
+";
+            SqlCommand command = null;
+            SqlDataReader dr = null;
+            try
+            {
+                command = DataBase.CreateBasicCommand(query);
+                command.Parameters.AddWithValue("@supplierID", id);
+                dr = DataBase.ExecuteDataReaderCommand(command);
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1}  Metodo Get Ejecutado Correctamente", DateTime.Now, Session.SessionID));
+                while (dr.Read())
+                {
+                    t = new Supplier(byte.Parse(dr[0].ToString()), dr[1].ToString(), dr[2].ToString(), dr[3].ToString(), dr[4].ToString(), dr[5].ToString(), dr[6].ToString(), short.Parse(dr[7].ToString()), DateTime.Parse(dr[8].ToString()), DateTime.Parse(dr[9].ToString()), byte.Parse(dr[10].ToString()));
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1} Error en el metodo Get de la Tabla Supplier - {2} - {3}", DateTime.Now, Session.SessionID, ex.Message, query));
+                throw ex;
+            }
+            finally
+            {
+                command.Connection.Close();
+                dr.Close();
+            }
+
+            return t;
+        }
+        public DataTable Selec()
+        {
+            DataTable dt = new DataTable();
+            System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1} Inicializando el Metodo Select de la Tabla Supplier", DateTime.Now, Session.SessionID));
+            string query = @"SELECT * FROM Supplier
+                                 ORDER BY 2";
+            try
+            {
+                SqlCommand command = DataBase.CreateBasicCommand(query);
+                dt = DataBase.ExecuteDataTableCommand(command);
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1}  Metodo Select Ejecutado Correctamente", DateTime.Now, Session.SessionID));
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1} Error en el metodo Select de la Tabla Supplier - {2} - {3}", DateTime.Now, Session.SessionID, ex.Message, query));
+                throw ex;
+            }
+            return dt;
+        }
+        public int Insert(Supplier t)
+        {
+            int res = 0;
+            System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1} Inicializando el Metodo Insert de la Tabla Supplier", DateTime.Now, Session.SessionID));
+            string query = @"INSERT INTO Supplier (supplierName, supplierLastName, supplierCI,direction,
+					cellphone,email,UserID,lastUpdate)
+                            VALUES (@supplierName,@supplierLastName,@supplierCI,@direction,
+					@cellphone,@email,@UserID,CURRENT_TIMESTAMP)";
+
+            try
+            {
+                SqlCommand command = DataBase.CreateBasicCommand(query);
+                command.Parameters.AddWithValue("@supplierName", t.SupplierName);
+                command.Parameters.AddWithValue("@supplierLastName", t.SupplierLastName);
+                command.Parameters.AddWithValue("@supplierCI", t.SupplierCI);
+                command.Parameters.AddWithValue("@direction", t.SupplierDirection);
+                command.Parameters.AddWithValue("@cellphone", t.SupplierCellphone);
+                command.Parameters.AddWithValue("@email", t.SupplierEmail);
+                command.Parameters.AddWithValue("@UserID", t.UserID);
+                res = DataBase.ExecuteBasicCommand(command);
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1}  Metodo Insert Ejecutado Correctamente", DateTime.Now, Session.SessionID));
+
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1} Error en el metodo Insert de la Tabla Supplier - {2} - {3}", DateTime.Now, Session.SessionID, ex.Message, query));
+                throw ex;
+            }
+            return res;
+        }
+        public DataTable SelectLikeByName(string txt)
+        {//No usado pero funcional
+            DataTable dt = new DataTable();
+            System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1} Inicializando el Metodo SelectLikeByName de la Tabla Supplier", DateTime.Now, Session.SessionID));
+            string query = @"SELECT C.supplierID,C.supplierCI, CONCAT(C.supplierLastName,' ',C.supplierName) AS 'Nombre Completo'
+                            FROM Supplier S
+                            WHERE (C.supplierCI LIKE @txt OR CONCAT(C.supplierLastName,' ',C.supplierName) LIKE @txt)";
+            try
+            {
+                SqlCommand command = DataBase.CreateBasicCommand(query);
+                command.Parameters.AddWithValue("@txt", "%" + txt + "%");
+                dt = DataBase.ExecuteDataTableCommand(command);
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1}  Metodo SelectLikeByName Ejecutado Correctamente", DateTime.Now, Session.SessionID));
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1} Error en el metodo SelectLikeByName de la Tabla Supplier - {2} - {3}", DateTime.Now, Session.SessionID, ex.Message, query));
+                throw ex;
+            }
+            return dt;
+        }
+
+        public int Update(Supplier t)
+        {
+            int res = 0;
+            System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1} Inicializando el Metodo Update de la Tabla Supplier", DateTime.Now, Session.SessionID));
+            string query = @"UPDATE Supplier SET supplierName=@supplierName, supplierLastName=@supplierLastName, supplierCI=@supplierCI,
+				              direction=@direction,cellphone=@cellphone ,email=@email,
+				              UserID=@UserID, lastUpdate = CURRENT_TIMESTAMP
+				              WHERE supplierID=@supplierID";
+
+            try
+            {
+                SqlCommand command = DataBase.CreateBasicCommand(query);
+                command.Parameters.AddWithValue("@supplierName", t.SupplierName);
+                command.Parameters.AddWithValue("@supplierLastName", t.SupplierLastName);
+                command.Parameters.AddWithValue("@supplierCI", t.SupplierCI);
+                command.Parameters.AddWithValue("@direction", t.SupplierDirection);
+                command.Parameters.AddWithValue("@cellphone", t.SupplierCellphone);
+                command.Parameters.AddWithValue("@email", t.SupplierEmail);
+                command.Parameters.AddWithValue("@UserID", t.UserID);
+                command.Parameters.AddWithValue("@supplierID", t.SupplierID);
+                res = DataBase.ExecuteBasicCommand(command);
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1}  Metodo Update Ejecutado Correctamente", DateTime.Now, Session.SessionID));
+
+            }
+            catch (Exception ex)
+            {
+                //Logs de errores!
+                System.Diagnostics.Debug.WriteLine(string.Format("{0} - User: {1} Error en el metodo Update de la Tabla Supplier - {2} - {3}", DateTime.Now, Session.SessionID, ex.Message, query));
+            }
+            return res;
+        }
+    }
+}
--- KapDB/DAO.Model/Supplier.cs
+++ KapDB/DAO.Model/Supplier.cs
@@ -5,7 +5,59 @@ using System.Text;
 
 namespace DAO.Model
 {
-    public class Supplier
+    public class Supplier:BaseModel
     {
+        #region Properties
+        public byte SupplierID { get; set; }
+        public string SupplierName { get; set; }
+        public string SupplierLastName { get; set; }
+        public string SupplierCI { get; set; }
+        public string SupplierDirection { get; set; }
+        public string SupplierCellphone { get; set; }
+        public string SupplierEmail { get; set; }
+
+        #endregion
+
+
+        public Supplier()
+        {
+
+        }
+
+
+
+        public Supplier(byte supplierID, string supplierName, string supplierLastName, string supplierCI, string supplierDirection, string supplierCellphone, string supplierEmail, short userID, DateTime registerDate, DateTime lastUpdate, byte status)
+            : base(status, registerDate, lastUpdate, userID)
+        {
+            SupplierID = supplierID;
+            SupplierName = supplierName;
+            SupplierLastName = supplierLastName;
+            SupplierCI = supplierCI;
+            SupplierDirection = supplierDirection;
+            SupplierCellphone = supplierCellphone;
+            SupplierEmail = supplierEmail;
+        }
+
+
+        /// <summary>
+        /// INSERT
+        /// </summary>
+        /// <param name="clientName"></param>
+        /// <param name="clientLastName"></param>
+        /// <param name="clientCI"></param>
+        /// <param name="clientDirection"></param>
+        /// <param name="clientTelephone"></param>
+        /// <param name="clientEmail"></param>
+        /// <param name="userID"></param>
+        public Supplier(string clientName, string clientLastName, string clientCI, string clientDirection, string clientTelephone, string clientEmail, short userID)
+        {
+            SupplierName = clientName;
+            SupplierLastName = clientLastName;
+            SupplierCI = clientCI;
+            SupplierDirection = clientDirection;
+            SupplierCellphone = clientTelephone;
+            SupplierEmail = clientEmail;
+            UserID = userID;
+        }
     }
 }
